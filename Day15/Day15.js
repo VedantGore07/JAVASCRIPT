0d982b35dbc0faed65064c3d3a0d232edc0fdfce
VedantGore07 <vedantgore713@gmail.com>
2025-10-04 20:43:09 +0530

    task_4 course enrollment portal

---
+++ Day15/Day15.js
@@ -0,0 +1,55 @@
+{/* <body>
+
+    <button onclick="askPermission()">Ask Permission</button>
+    <script>
+        function askPermission(){
+            Notification.requestPermission().then(permission=>{
+                alert("Permission is" +permission)
+            })
+        }
+    </script>
+</body> */}
+
+
+
+
+
+
+
+
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//   <title>Reminder App</title>
+// </head>
+// <body>
+//   <h2>Reminder App</h2>
+//   <button onclick="startReminders()">Start Reminders</button>
+
+
+//   <script>
+//     function startReminders() {
+//       if(Notification.permission === 'granted'){
+//            sendReminders();
+
+
+//         }else{
+//             alert("Please allow notification first")
+//         }
+//     }
+        
+    
+//     function sendReminders() {
+//       setInterval(() => {
+//         new Notification("Reminder!", {
+//           body: "Take break from the screen please",
+//         });
+//       }, 5000); 
+//     }
+//   </script>
+// </body>
+// </html>
+
